9f651b2d82588535298502eaa79ec947cd2a4c36
Sage Farinas <sagefarinas20@gmail.com>
2023-09-07 13:47:13 -0500

    Creating new branch

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home({ allPostsData }) {
       <section className={utilStyles.headingMd}>
         <p>Hello, I'm Sage. I'm a full-stack engineer.</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          (This is a sample website - we'll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
